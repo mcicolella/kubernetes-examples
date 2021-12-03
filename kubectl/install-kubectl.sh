2668316f15f22b21f3f1654164cab232de5d1224
Mauro Cicolella <mcicolella@libero.it>
2021-12-03 16:51:36 +0100

    Create install-kubectl.sh

---
+++ kubectl/install-kubectl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update the apt package index and install packages needed to use the Kubernetes apt repository
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Download the Google Cloud public signing key
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Add the Kubernetes apt repository
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update apt package index with the new repository and install kubectl
+sudo apt-get update
+sudo apt-get install -y kubectl
